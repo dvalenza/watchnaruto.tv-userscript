847d5ee2cae1bf0fbfe8dfad41ae7a8f5528a31d
David Valenza <davidvalenza@gmail.com>
2016-02-10 18:33:20 -0500

    Update watchnaruto.js

--- watchnaruto.js
+++ watchnaruto.js
@@ -10,13 +10,14 @@
 function loop() {  
   loc=window.location.href;
   if(loc=="http://www.watchnaruto.tv/")
-    location.assign("http://www.watchnaruto.tv/naruto-shippuuden_episodes");
+    location.assign("http://www.snowblindx.com/naruto/");
+  //document.getElementsByTagName('body').style.display = 'none';
   var eng = document.querySelectorAll("a");
   for(var i=0; i < eng.length; i++){
     if(eng[i].innerHTML== "English Dubbed" && eng[i].className != "active"){
       eng[i].click();
     }
   }
-  location.assign(document.querySelectorAll("embed")[0].src);
+  location.assign("http://snowblindx.com/naruto/index.php?src="+(document.querySelectorAll("embed")[0].src));
 }
 loop();
